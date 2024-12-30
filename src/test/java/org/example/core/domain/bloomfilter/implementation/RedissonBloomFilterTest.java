2b1ff40f0e1917cea4b9ee8c63b07825fa43c6a8
Darren <carricanbuild@gmail.com>
2024-12-30 17:36:08 -0500

    modify bloom filter test

--- src/test/java/org/example/core/domain/bloomfilter/implementation/RedissonBloomFilterTest.java
+++ src/test/java/org/example/core/domain/bloomfilter/implementation/RedissonBloomFilterTest.java
@@ -2,10 +2,13 @@ package org.example.core.domain.bloomfilter.implementation;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.Stream;
 import org.example.container.RedisBaseTest;
 import org.example.core.configuration.bloomfilter.BloomFilterConfigProperties;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
@@ -22,14 +25,23 @@ class RedissonBloomFilterTest extends RedisBaseTest {
     redissonClient = Redisson.create(config);
   }
 
-  @Test
-  void test_Add() {
+  private static final String BLOOM_FILTER_KEY = "key";
+
+  private static Stream<Arguments> provide_Happy_Case_Arguments() {
+    return Stream.of(
+        Arguments.of(BLOOM_FILTER_KEY, "cat", true),
+        Arguments.of(BLOOM_FILTER_KEY, "dog", false)
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("provide_Happy_Case_Arguments")
+  void happy_passes(String bloomFilterKey, String value, boolean existed) {
     BloomFilterConfigProperties properties = new BloomFilterConfigProperties();
     properties.setExpectedSize(1000L);
     properties.setFalsePositiveRate(0.001D);
     RedissonBloomFilter redissonBloomFilter = new RedissonBloomFilter(properties, redissonClient);
-    redissonBloomFilter.add("key", "twen");
-    assertThat(redissonBloomFilter.contains("key", "twen")).isTrue();
-    assertThat(redissonBloomFilter.contains("key", "water-tiger-twen")).isFalse();
+    redissonBloomFilter.add(BLOOM_FILTER_KEY, "cat");
+    assertThat(redissonBloomFilter.contains(bloomFilterKey, value)).isEqualTo(existed);
   }
 }
